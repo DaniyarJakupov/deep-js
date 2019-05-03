d42d64182e895e31e9d47974379cb699b2490cef
Daniyar Jakupov <ddr199305@gmail.com>
2019-05-03 08:48:37 +0200

    Fix comment

--- types-exercises/object-is/ex.js
+++ types-exercises/object-is/ex.js
@@ -1,24 +1,49 @@
 // TODO: define polyfill for `Object.is(..)`
+// Object.is fixes 2 cases of triple equal:
+// 1) Correctly checks equality of negative zero (-0) with zero (0)
+// 2) Correctly checks equality of NaN
+if (!Object.is || true) {
+  Object.is = function ObjectIs(x, y) {
+    var xNegZero = isItNegZero(x);
+    var yNegZero = isItNegZero(y);
 
+    if (xNegZero || yNegZero) {
+      return xNegZero && yNegZero;
+    } else if (isItNaN(x) && isItNaN(y)) {
+      return true;
+    } else if (x === y) {
+      return true;
+    }
 
+    return false;
+
+    /* Helper functions */
+    function isItNegZero(x) {
+      return x === 0 && 1 / x === -Infinity;
+    }
+    function isItNaN(x) {
+      return x !== x; // NaN is the only value that does not equal to itself
+    }
+  };
+}
 
 // tests:
-console.log(Object.is(42,42) === true);
-console.log(Object.is("foo","foo") === true);
-console.log(Object.is(false,false) === true);
-console.log(Object.is(null,null) === true);
-console.log(Object.is(undefined,undefined) === true);
-console.log(Object.is(NaN,NaN) === true);
-console.log(Object.is(-0,-0) === true);
-console.log(Object.is(0,0) === true);
+console.log(Object.is(42, 42) === true);
+console.log(Object.is("foo", "foo") === true);
+console.log(Object.is(false, false) === true);
+console.log(Object.is(null, null) === true);
+console.log(Object.is(undefined, undefined) === true);
+console.log(Object.is(NaN, NaN) === true);
+console.log(Object.is(-0, -0) === true);
+console.log(Object.is(0, 0) === true);
 
-console.log(Object.is(-0,0) === false);
-console.log(Object.is(0,-0) === false);
-console.log(Object.is(0,NaN) === false);
-console.log(Object.is(NaN,0) === false);
-console.log(Object.is(42,"42") === false);
-console.log(Object.is("42",42) === false);
-console.log(Object.is("foo","bar") === false);
-console.log(Object.is(false,true) === false);
-console.log(Object.is(null,undefined) === false);
-console.log(Object.is(undefined,null) === false);
+console.log(Object.is(-0, 0) === false);
+console.log(Object.is(0, -0) === false);
+console.log(Object.is(0, NaN) === false);
+console.log(Object.is(NaN, 0) === false);
+console.log(Object.is(42, "42") === false);
+console.log(Object.is("42", 42) === false);
+console.log(Object.is("foo", "bar") === false);
+console.log(Object.is(false, true) === false);
+console.log(Object.is(null, undefined) === false);
+console.log(Object.is(undefined, null) === false);
